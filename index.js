5bdcae9c3259f91729fe9fbaa1cc6b0ed63b9234
Amy Hawthorne <amy.hawthorne@gmail.com>
2020-09-11 16:56:42 -0700

    upload for mentor review and debugging

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const STORE = {
                 d: "Jim",
 
             },
-            correctAnswer: "c",
+            correctAnswer: "Devon",
             correctAnswerImage: "question_1_right.gif",
             correctAnswerAlt: "An animated gif of Michael Scott with a serious face",
             incorrectAnswerImage: "wrong-answer-dwight.gif",
@@ -89,16 +89,16 @@ function renderQuiz(STORE) {
         $('.completed-quiz-score').hide();
     } else {
         //Generates HTML to render quiz questions
-        const answerOptionsHtml =  `<input type="radio" name="option" id="option" 
+        const answerOptionsHtml =  `<input type="radio" name="option"  
             value="${STORE.currentQuestion.answers.a}" required></input>
             <label for "${STORE.currentQuestion.answers.a}">"${STORE.currentQuestion.answers.a}"</label><br>
-            <input type="radio" name="option" id="option" 
+            <input type="radio" name="option"  
             value="${STORE.currentQuestion.answers.b}" required></input>
             <label for "${STORE.currentQuestion.answers.b}">"${STORE.currentQuestion.answers.b}"</label><br>
-            <input type="radio" name="option" id="option" 
+            <input type="radio" name="option"  
             value="${STORE.currentQuestion.answers.c}" required></input>
             <label for "${STORE.currentQuestion.answers.c}">"${STORE.currentQuestion.answers.c}"</label><br>
-            <input type="radio" name="option" id="option" 
+            <input type="radio" name="option"  
             value="${STORE.currentQuestion.answers.d}" required></input>
             <label for "${STORE.currentQuestion.answers.d}">"${STORE.currentQuestion.answers.d}"</label><br>`
         const questionHtml = `<div>
@@ -141,37 +141,55 @@ function renderProgress() {
 
 function renderScore() {
     console.log('RenderScore has rendered');
-
-    //generates html for message that appears based on whether selected answer is correct or incorrect
-    const feedbackIfCorrectHtml = `Your answer is correct! Call the party planning committee and order whatever Angela permits you to order! (Please stay within budget)
-        <img class="Correct-Answer" src="${STORE.remainingQuestions.correctAnswerImage}" alt="${STORE.remainingQuestions.correctAnswerAlt}" />
-        <button type="button" id="next-question-button">Next Question</button>`
-    const feedbackIfIncorrectHtml = `Your answer is incorrect. As Michael would say: It feels like somebody took my heart and dropped it into a bucket of boiling tears.
-        <img class="Incorrect-Answer" src="${STORE.remainingQuestions.incorrectAnswerImage}" alt="${STORE.remainingQuestions.incorrectAnswerAlt}"` 
-    //determine whether or not an answer is correct or incorrect and logs it to the STORE, calculating the total correct score and incorrect score
     let correctAnswerTally = (STORE.score.correctAnswers);
     let incorrectAnswerTally = (STORE.score.incorrectAnswers);
-    let selected = $('#input:checked');
-    let answer = selected.val();
+
+    //generates html for message that appears based on whether selected answer is correct or incorrect
+    const feedbackIfCorrectHtml = `Your answer is correct! Call the party planning committee and order whatever 
+    Angela permits you to order! (Please stay within budget)
+        <img class="Correct-Answer" src="${STORE.currentQuestion.correctAnswerImage}" alt="${STORE.currentQuestion.correctAnswerAlt}" />
+        <button type="button" id="next-question-button">Next Question</button>`
+    const feedbackIfIncorrectHtml = `Your answer is incorrect. As Michael would say: It feels like somebody took my heart and dropped it into a bucket of boiling tears.
+        <img class="Incorrect-Answer" src="${STORE.currentQuestion.incorrectAnswerImage}" alt="${STORE.currentQuestion.incorrectAnswerAlt}"` 
+    
+        //determine whether or not an answer is correct or incorrect and logs it to the STORE, calculating the total correct score and incorrect score
+    
     $('#submit-button').on('click', () => {
         event.preventDefault();
         $('.show-question.option').required = true;
-        for (let answer in STORE.remainingQuestions.answers) {
-            if (answer === '${STORE.remainingQuestions.correctAnswer}') {
-            correctAnswerTally = (STORE.score.correctAnswers + 1);
-            console.log("A correct answer was counted");
-            $('.feedback-on-answer').html(feedbackIfCorrectHtml);
-            } 
-            else {
-            incorrectAnswerTally = (STORE.score.incorrectAnswers + 1);
-            console.log("An incorrect answer was counted");
-            $('.feedback-on-answer').html(feedbackIfIncorrectHtml);
-            }
+        const selected = $('input:checked');
+        let answer = selected.val();
+        console.log(answer + ` was selected`);
+        let correctAnswerFound = false;
+        
+        for (let answerKey in STORE.currentQuestion.answers) {
+            let answerValue = STORE.currentQuestion.answers[answerKey]; 
+            if (answerValue === STORE.currentQuestion.correctAnswer) {
+            correctAnswerFound === true;
+            console.log(answerValue);
+            console.log(STORE.currentQuestion.correctAnswer);
+            };
         };
+
+        if (correctAnswerFound === true) {
+            let correctAnswerTally = (STORE.score.correctAnswers + 1);
+            console.log(correctAnswerTally + ` is the number of correct answers counted`);
+            console.log(`A correct answer was counted`);
+            console.log(answerValue);
+            $('.feedback-on-answer').html(feedbackIfCorrectHtml);
+        } 
+        else {
+            incorrectAnswerTally = (STORE.score.incorrectAnswers++);
+            //console.log("An incorrect answer was counted");
+            console.log(incorrectAnswerTally + ` is the number of incorrect answers counted`);
+            $('.feedback-on-answer').html(feedbackIfIncorrectHtml);
+        };
+       
         //generates score html
         const renderScoreHtml = `${correctAnswerTally} answers correct out of 30`;
         $('.show-score').html(renderScoreHtml);
         console.log($('.show-score').length);
+        console.log(`Show Score has rendered`);
         $('.show-question').hide();
         $('.feedback-on-answer').show();
         console.log("submit has been clicked");
@@ -183,9 +201,10 @@ function renderFinalScore() {
     console.log('Final Score has rendered');
     //generates html to show user their final score once the 30 questions have all been answered. 
     let correctAnswerTally = (STORE.score.correctAnswers + 1);
+    let incorrectAnswerTally = (STORE.score.incorrectAnswers + 1);
     let renderFinalScorePoorHtml = `You got ${correctAnswerTally} out of 30 answers correct! Too much time in the breakroom. Back to work! 
     <button type="button" id="try-again">Try Again</button>`
-    $('.completed-quiz-score').html(renderFinalScoreHtml);
+    $('.completed-quiz-score').html(renderFinalScorePoorHtml);
     let renderFinalScoreAverageHtml = `You got ${correctAnswerTally} out of 30 answers correct! Not bad! You could soon be named assistant to the regional manager.`
     let renderFinalScoreGoodHtml = `You got ${correctAnswerTally} out of 30 answers correct! David Wallace is calling! You're about to receive a promotion from Corporate.`
     console.log($('.completed-quiz-score').length);
